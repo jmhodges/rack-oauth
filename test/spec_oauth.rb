5818a438548a7e0a2e3f9a8c397770c20ffc9a52
Jeff Hodges <jeff@somethingsimilar.com>
2009-09-18 00:22:20 -0700

    tabbing issues and renaming the singular test.

--- test/spec_oauth.rb
+++ test/spec_oauth.rb
@@ -18,49 +18,49 @@ class OAuth::Consumer
   end
 end
 
-  def app(hsh={})
-    inner_lint = Rack::Lint.new(lambda { |env|
-                                  [200,
-                                   {"Content-type" => "test/plain",
-                                     "Content-length" => "3"
-                                   },
-                                   ["foo"]
-                                  ]
-                                })
-    oauth = Rack::OAuth.new(inner_lint, DEFAULT.merge(hsh))
-    Rack::Session::Cookie.new(Rack::Lint.new(oauth))
-  end
+def app(hsh={})
+  inner_lint = Rack::Lint.new(lambda { |env|
+                                [200,
+                                 {"Content-type" => "test/plain",
+                                   "Content-length" => "3"
+                                 },
+                                 ["foo"]
+                                ]
+                              })
+  oauth = Rack::OAuth.new(inner_lint, DEFAULT.merge(hsh))
+  Rack::Session::Cookie.new(Rack::Lint.new(oauth))
+end
 
-  def mock_http_response(code, body)
-    mock('Net::HTTP') do |m|
-      stubs('code').returns(code.to_s)
-      stubs('body').returns(body)
-      stubs('error!').returns do
-        raise Net::HTTP::CODE_TO_OBJ[code.to_s].new("shoot: #{code}", self)
-      end
+def mock_http_response(code, body)
+  mock('Net::HTTP') do |m|
+    stubs('code').returns(code.to_s)
+    stubs('body').returns(body)
+    stubs('error!').returns do
+      raise Net::HTTP::CODE_TO_OBJ[code.to_s].new("shoot: #{code}", self)
     end
   end
+end
 
-  def twitter_request_token_response
-    body = "oauth_token=good_oauth_token&oauth_token_secret=good_token_secret&oauth_callback_confirmed=true"
-    mock_http_response(200, body)
-  end
+def twitter_request_token_response
+  body = "oauth_token=good_oauth_token&oauth_token_secret=good_token_secret&oauth_callback_confirmed=true"
+  mock_http_response(200, body)
+end
 
-  
+
 context 'Rack::OAuth' do
 
   context 'on login' do
     setup do
-    OAuth::Consumer.
+      OAuth::Consumer.
         any_instance.
         expects(:request).
         with(:post, "/oauth/request_token", nil, {:oauth_callback=>"http:/oauth_callback"}).
         returns(twitter_request_token_response)
     end
     
-    specify ', on login, redirects the User to the Service Provider’s User Authorization URL' do
+    specify 'redirects the User to the Service Provider’s User Authorization URL' do
       res = Rack::MockRequest.new(app).get('/oauth_login')
-    res.should.be.redirect
+      res.should.be.redirect
     end
   end
 end
